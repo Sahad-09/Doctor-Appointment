6cf479a6a8c5e381be8d3cc7aa3ffdf36f091018
Sahad <msahad124@gmail.com>
2023-12-18 21:07:57 +0530

    Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,54 @@
+import { NavLink } from "react-router-dom";
+
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/doctors",
+    display: "Home",
+  },
+  {
+    path: "/home",
+    display: "Find a doctor",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+  {
+    path: "/login",
+    display: "Login",
+  },
+];
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <nav className=" pt-3">
+        <ul className=" flex gap-10 justify-center">
+          {navLinks.map((link, index) => {
+            return (
+              <li key={index}>
+                <NavLink to={link.path}>
+                  {link.display == "Login" ? (
+                    <button className=" bg-slate-500">{link.display}</button>
+                  ) : (
+                    link.display
+                  )}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Header;
